92f1ab3129eb144b2f05f063b121c7c1ef1141d0
qq762538930 <762538930@qq.com>
2022-12-13 17:00:15 +0800

    GridView.builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
@@ -15,11 +17,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: '王者荣耀英雄榜',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: '王者荣耀英雄榜'),
     );
   }
 }
@@ -33,6 +35,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<Heroes> heros = [];
+  int hero_num = 0;
 
   Future getHeros() async {
     var response =
@@ -40,7 +43,9 @@ class _MyHomePageState extends State<MyHomePage> {
     // var jsonData = jsonDecode(response.body);
     var jsonData = json.decode(utf8.decode(response.bodyBytes));
     // print(jsonData[0]['cname']);
-    // print(jsonData.length);//112
+    //112
+    hero_num=jsonData.length;
+
 
     for (var eachTeam in jsonData) {
       // print(eachTeam);
@@ -52,41 +57,50 @@ class _MyHomePageState extends State<MyHomePage> {
       // print('${heroes.title}-${heroes.cname}');
       // print(heroes.title);
       heros.add(heroes);
+      // print(jsonData.length);//112
+      // print(jsonData.length);
+      // print(heros.length);
 
       // print(heros);
     }
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('123'),
+        title: const Text('王者荣耀英雄榜'),
       ),
       body: SafeArea(
         child: FutureBuilder(
             future: getHeros(),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.done) {
-                return ListView.builder(
-                  itemCount: heros.length,
-                  padding: const EdgeInsets.all(8),
+                return GridView.builder(
+                  itemCount: hero_num,
+                  padding: const EdgeInsets.all(10),
                   itemBuilder: (context, index) {
                     return Padding(
-                      padding: const EdgeInsets.all(8.0),
+                      padding: const EdgeInsets.all(1.0),
                       child: Container(
                         decoration: BoxDecoration(
-                          color: Colors.grey[200],
+                          // color: Colors.yellowAccent,
                           borderRadius: BorderRadius.circular(8),
                         ),
                         child: ListTile(
-                          title: Text(heros[index].title),
+                          // title: const Text('1'),
+                          title: Image.network(
+                              'https://game.gtimg.cn/images/yxzj/img201606/heroimg/${heros[index].ename}/${heros[index].ename}.jpg'),
                           subtitle: Text(heros[index].cname),
                         ),
                       ),
                     );
                   },
+                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 100,
+                      childAspectRatio: 1 / 1,
+                      crossAxisSpacing: 40,
+                      mainAxisSpacing: 10),
                 );
               } else {
                 return const Center(child: CircularProgressIndicator());
